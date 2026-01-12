ea9dd87bc2c1b0dd2e9aa967b274f3b187a15cbe
Lucas Huang <lucas.huang@menloschool.org>
2026-01-11 21:00:11 -0800

    Completed

--- src/Boggle.java
+++ src/Boggle.java
@@ -10,15 +10,12 @@ public class Boggle {
         for (int i = 0; i < dictionary.length; i++) {
             tst.insert(dictionary[i]);
         }
-
+        boolean[][] visited = new boolean[board.length][board[0].length];
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
-                boolean[][] visited = new boolean[board.length][board[0].length];
                 DFS(tst, board, visited, goodWords, i, j, "");
             }
         }
-        // TODO: Complete the function findWords(). Add all words that are found both on the board
-        //  and in the dictionary.
 
         // Convert the list into a sorted array of strings, then return the array.
         String[] sol = new String[goodWords.size()];
